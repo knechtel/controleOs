7823945dc5d5bf421fff1e0853cb393bc6e8cbb2
maiquelknechtel1 <maiquelknechtel@gmail.com>
2024-09-09 14:34:30 -0300

    Adicionando modelo de equipamento.

---
+++ desktop/model/equipment.py
@@ -0,0 +1,13 @@
+class Equipment:
+        def __init__(self):
+            self.id = 0
+            self.brand =''
+            self.model = ''
+            self.serial = ''
+            self.defectForRepair = ''
+            self.price = 0.0
+            self.obs = ''
+            self.autorizado = False
+            self.devolucao = False
+            self.pronto = False
+            self.entregue = False

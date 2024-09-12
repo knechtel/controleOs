8b9964729b827499e5c4c22da472b875f6fad5c5
maiquelknechtel1 <maiquelknechtel@gmail.com>
2024-09-12 07:40:00 -0300

    Refatorando retorno de client_find_all.

--- desktop/service/client_service.py
+++ desktop/service/client_service.py
@@ -1,10 +1,47 @@
 import requests
+from model.client import Client
+from model.equipment import Equipment
+def is_iterable(obj):
+    try:
+        iter(obj)
+        return True
+    except TypeError:
+        return False
 
 def client_find_all():
+    list_client = []
     x = requests.get('http://localhost:8080/client-findAll')
     data = x.json()
-    print(data[1]["id"])
-    return data
+    
+    for index in  data:
+        client = Client()
+        client.id = index["id"]
+        client.name = index["name"]
+        client.cpf = index["cpf"]
+        client.email = index["email"]
+        client.endereco = index["address"]
+        client.telefone = index["phone"]
+        if(is_iterable(index["equipments"])):
+            for e1 in  index["equipments"]:
+                equipment = Equipment()
+                equipment.id = e1["id"]
+                equipment.model = e1["model"]
+                equipment.serial = e1["serial"]
+                equipment.brand =  e1["brand"]
+                equipment.price =  e1["price"]
+                equipment.pronto = e1["pronto"]
+                equipment.autorizado = e1["autorizado"]
+                equipment.entryDate =e1["entryDate"]
+                equipment.defectForRepair = e1["defectForRepair"]
+                equipment.departureDate = e1["departureDate"]
+                equipment.obs = e1["obs"]
+                equipment.devolucao = e1["devolucao"]
+                client.list_equipments.append(equipment)
+                print("item obs = ",equipment.obs," id = ",equipment.id)
+        list_client.append(client)
+        #print(client.list_equipments)
+    
+    return list_client
 
 def client_find_by_id(id):
     url = 'http://localhost:8080/client-findById'
@@ -33,9 +70,11 @@ def client_create(client):
     myobj = { 'name':client.name,
              'email':client.email,
              'cpf':client.cpf,
-             'phone':client.telefone
+             'phone':client.telefone,
     }
     x = requests.post(url, json = myobj)
   
     print('teste = ',x)
     return x.json().get('id')
+
+client_find_all()

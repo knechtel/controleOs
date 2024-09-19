f6f207d1225fd807218a21699e8c3a3b3e0cf2ec
maiquelknechtel1 <maiquelknechtel@gmail.com>
2024-09-19 12:12:55 -0300

    Atualizando DTO.

--- server/src/main/java/com/dular/demo/DTO/EquipmentDto.java
+++ server/src/main/java/com/dular/demo/DTO/EquipmentDto.java
@@ -1,5 +1,8 @@
 package com.dular.demo.DTO;
 import com.dular.demo.domain.Equipment;
+
+import java.util.Date;
+
 /**
  * Created by maiquelknechtel on 12/07/24.
  */
@@ -14,8 +17,14 @@ public class EquipmentDto {
     private Integer idClient;
     private Boolean pronto;
     private Boolean autorizado;
-    private String entregue;
+    private Boolean entregue;
+    private Boolean garantia;
+    private Boolean devolucao;
+    private String obs;
+    private String description;
+    private Boolean departuretWarranty;
 
+    private Double price;
     public Integer getId() {
         return id;
     }
@@ -71,9 +80,10 @@ public class EquipmentDto {
 
         entity.setModel(model);
         entity.setSerial(serial);
-
-
-
+        entity.setBrand(brand);
+        entity.setDevolucao(devolucao);
+        entity.setObs(obs);
+        entity.setPrice(price);
         return entity;
     }
 
@@ -81,28 +91,52 @@ public class EquipmentDto {
         Equipment entity = new Equipment();
         entity.setId(id);
         if (brand != null) {
-
+            entity.setBrand(brand);
+        }
+        if (price != null) {
+            entity.setPrice(price);
         }
         if (model != null) {
-
+            entity.setModel(model);
         }
         if (serial != null) {
-
+            entity.setSerial(serial);
         }
         if (costValue != null) {
 
         }
         if (defectForRepair != null) {
-
+            entity.setDefectForRepair(defectForRepair);
         }
+
+
         if (pronto != null) {
             System.out.println("toBuild");
             System.out.println(pronto);
-
+            entity.setPronto(pronto);
         }
         if (autorizado != null) {
-
+            entity.setAutorizado(autorizado);
         }
+        if (devolucao != null) {
+            entity.setDevolucao(devolucao);
+            System.out.println("devolucao entrei aqui ... = "+devolucao);
+        }
+        if (entregue!=null) {
+            entity.setEntregue(entregue);
+            entity.setDepartureDate(new Date());
+        }
+        if(garantia!=null){
+            entity.setGarantia(garantia);
+            //entity.setDepartureEquipmentWarranty(new Date());
+        }
+        if(departuretWarranty!=null&&departuretWarranty==true)
+            entity.setDepartureEquipmentWarranty(new Date());
+
+        entity.setDescription(description);
+
+        entity.setObs(obs);
+
         return entity;
     }
 
@@ -130,12 +164,76 @@ public class EquipmentDto {
         this.autorizado = autorizado;
     }
 
-    public String getEntregue() {
+    public Boolean getEntregue() {
         return entregue;
     }
 
-    public void setEntregue(String entregue) {
+    public void setEntregue(Boolean entregue) {
         this.entregue = entregue;
+    }
+
+    public Boolean getDevolucao() {
+        return devolucao;
+    }
+
+    public void setDevolucao(Boolean devolucao) {
+        this.devolucao = devolucao;
+    }
+
+    public String getObs() {
+        return obs;
+    }
+
+    public void setObs(String obs) {
+        this.obs = obs;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Boolean getPronto() {
+        return pronto;
+    }
+
+    public void setPronto(Boolean pronto) {
+        this.pronto = pronto;
+    }
+
+    public Boolean getAutorizado() {
+        return autorizado;
+    }
+
+    public void setAutorizado(Boolean autorizado) {
+        this.autorizado = autorizado;
+    }
+
+    public Boolean getGarantia() {
+        return garantia;
+    }
+
+    public void setGarantia(Boolean garantia) {
+        this.garantia = garantia;
+    }
+
+    public Boolean getDeparturetWarranty() {
+        return departuretWarranty;
+    }
+
+    public void setDeparturetWarranty(Boolean departuretWarranty) {
+        this.departuretWarranty = departuretWarranty;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
 

6c072ab4133141a596523757d378be8212712744
maiquelknechtel1 <maiquelknechtel@gmail.com>
2024-09-21 14:03:49 -0300

    Atualizando submódulo controleDeOs para o novo commit

--- component/FormEquipment.js
+++ component/FormEquipment.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import {
   View,
   TextInput,
@@ -8,15 +9,47 @@ import {
   StyleSheet,
   ScrollView,
 } from "react-native";
+import { FIND_BY_ID_CLIENT, FIND_BY_ID_EQUIPMENT } from "../util/urls";
 
-const FormEquipment = ({ navigation }) => {
+const FormEquipment = ({ route, navigation }) => {
   const [descricao, setDescricao] = useState("");
   const [modelo, setModelo] = useState("");
   const [serial, setSerial] = useState("");
   const [marca, setMarca] = useState("");
   const [defeito, setDefeito] = useState("");
   const [preco, setPreco] = useState("");
-
+  // useEffect será executado quando o componente for montado
+  useEffect(() => {
+    // Código que será executado na inicialização do componente
+    console.log("Componente montado ! " + route.params.paramKey);
+    idClient = route.params.paramKey;
+    fetch(FIND_BY_ID_CLIENT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: idClient,
+      }),
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log("=================================================");
+        console.log("=================================================");
+        console.log(json["equipments"][0]["id"]);
+        console.log("=================================================");
+        console.log("=================================================");
+      });
+    // O retorno do useEffect (opcional) seria executado quando o componente for desmontado
+    return () => {
+      console.log("Componente desmontado!");
+    };
+  }, []); // O array vazio [] faz com que o useEffect seja executado apenas uma vez, na montagem.
+  // Por exemplo, poderia ser uma chamada de API aqui
+  const carregarDados = () => {
+    // Simulação de uma função que carrega dados
+    console.log("Carregando dados...");
+  };
   const handleSubmit = () => {
     // Aqui você pode adicionar a lógica para enviar os dados do formulário para uma API ou banco de dados
   };
--- util/urls.js
+++ util/urls.js
@@ -1,3 +1,4 @@
 export const LOCAL = "http://10.0.0.199:8080";
 
 export const FIND_ALL_CLIENT = LOCAL + "/client-findAll";
+export const FIND_BY_ID_CLIENT = LOCAL + "/client-findById";

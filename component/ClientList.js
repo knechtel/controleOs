29afac6dc3c54f9831cc2b8ff0fa26060ed8cf59
maiquelknechtel1 <maiquelknechtel@gmail.com>
2024-09-24 20:12:37 -0300

    Modificando interface com lista de clientes.

--- component/ClientList.js
+++ component/ClientList.js
@@ -7,18 +7,26 @@ import {
   StyleSheet,
   RefreshControl,
   Button,
+  Animated,
+  View,
+  Image, // Adicionei o componente Image
+  Dimensions,
 } from "react-native";
 import { FIND_ALL_CLIENT } from "../util/urls";
 import axios from "axios";
+const HEADER_MAX_HEIGHT = 120;
+const HEADER_MIN_HEIGHT = 70;
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 class ClientList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       client: [],
       refreshing: false,
+      scrollY: new Animated.Value(0),
     };
   }
-  redirectToEdit = (id) => {
+  redirectToEditEquipment = (id) => {
     const { navigation } = this.props;
     navigation.navigate("FormClient", { paramKey: id });
   };
@@ -29,7 +37,7 @@ class ClientList extends Component {
         client: response.data,
       });
       this.setState({ refreshing: false });
-      console.log(response.data);
+      //console.log(response.data);
     });
   };
 
@@ -38,42 +46,75 @@ class ClientList extends Component {
       this.setState({
         client: response.data,
       });
-      console.log(response.data);
+      //console.log(response.data);
     });
   }
   alertItemName = (item) => {
-    alert(item.name);
     this.setState({ refreshing: false });
-    this.redirectToEdit(item.id);
+    this.redirectToEditEquipment(item.id);
   };
   render() {
+    const headerHeight = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+      outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+      extrapolate: "clamp", // Impede que o valor vá além dos limites definidos
+    });
+
+    const headerOpacity = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+      outputRange: [1, 0],
+      extrapolate: "clamp",
+    });
     return (
       <>
-        <ScrollView
-          refreshControl={
-            <RefreshControl
-              refreshing={this.state.refreshing}
-              onRefresh={this._onRefresh}
+        <View style={styles.container}>
+          {/* Cabeçalho animado */}
+          <Animated.View
+            style={[
+              styles.header,
+              { height: headerHeight, opacity: headerOpacity },
+            ]}
+          >
+            <Image
+              source={require("../assets/eletronica1.png")} // Substitua pela URL da sua imagem ou use require para imagens locais
+              style={styles.headerImage} // Estilo da imagem
             />
-          }
-        >
-          {this.state.client.map((item, index) => (
-            <TouchableOpacity
-              key={item.id}
-              style={styles.container}
-              onPress={() => this.alertItemName(item)}
-            >
-              <Text style={styles.text}>
-                {item.name} - {item.id}{" "}
-              </Text>
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
-        <Button
-          onPress={this.redirectToHome}
-          title="Adicionar equipamento"
-          color="#841584"
-        />
+          </Animated.View>
+
+          {/* ScrollView animado */}
+          {/* ScrollView animado */}
+          <Animated.ScrollView
+            style={styles.scrollView}
+            onScroll={Animated.event(
+              [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }],
+              { useNativeDriver: false } // Necessário para o ScrollView
+            )}
+            scrollEventThrottle={16}
+            refreshControl={
+              <RefreshControl
+                refreshing={this.state.refreshing}
+                onRefresh={this._onRefresh}
+              />
+            }
+          >
+            {this.state.client.map((item, index) => (
+              <TouchableOpacity
+                key={item.id}
+                style={styles.squareContainer}
+                onPress={() => this.alertItemName(item)}
+              >
+                <Text style={styles.text}>
+                  {item.name} - {item.id}{" "}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </Animated.ScrollView>
+          <Button
+            onPress={this.redirectToHome}
+            title="Adicionar equipamento"
+            color="#841584"
+          />
+        </View>
       </>
     );
   }
@@ -83,12 +124,57 @@ export default ClientList;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
+    padding: 15,
     marginTop: 3,
     backgroundColor: "#d9f9b1",
-    alignItems: "center",
+    flex: 1,
   },
-  text: {
-    color: "#4f603c",
+  squareContainer: {
+    alignItems: "center",
+    backgroundColor: "#d9f9b1",
+    borderRadius: 10, // Borda arredondada
+    padding: 15,
+    marginVertical: 5, // Espaço entre os quadrados
+    marginHorizontal: 10, // Margem nas laterais
+    elevation: 3, // Sombra para Android
+    shadowColor: "#000", // Sombra para iOS
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+  },
+  header: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "dodgerblue",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1,
+  },
+  headerImage: {
+    width: SCREEN_WIDTH, // Largura da imagem
+    height: HEADER_MAX_HEIGHT, // Altura da imagem
+    //  borderRadius: 25, // Deixa a imagem redonda, caso seja quadrada
+    //  marginBottom: 10, // Espaço entre a imagem e o texto
+  },
+  headerText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  scrollView: {
+    marginTop: HEADER_MAX_HEIGHT, // Para evitar que o conteúdo fique atrás do cabeçalho
+  },
+
+  content: {
+    padding: 20,
+  },
+  textContent: {
+    fontSize: 16,
+    marginVertical: 20,
   },
 });
